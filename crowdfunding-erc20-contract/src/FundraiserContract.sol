b316567e637484784a1ca5d1e84e882fb0d388e5
Esteban Pintos <esteban.pintos@wetransfer.com>
2025-01-28 15:19:27 +0100

    Implements deposit

--- crowdfunding-erc20-contract/src/FundraiserContract.sol
+++ crowdfunding-erc20-contract/src/FundraiserContract.sol
@@ -8,6 +8,15 @@ contract FundraiserContract is IFundraiser {
     /// ERRORS
     error FundraiserContract__DeadlineCannotBeInThePast();
     error FundraiserContract__GoalCannotBeZero();
+    error FundraiserContract__DepositCannotBeZero();
+    error FundraiserContract__FundraiserDoesNotExist();
+    error FundraiserContract__CannotDepositAfterDeadline();
+
+    enum FundraiserState {
+        NOT_CREATED,
+        CREATED,
+        GOAL_MET
+    }
 
     // TYPE DECLARATIONS
     struct Fundraiser {
@@ -17,6 +26,7 @@ contract FundraiserContract is IFundraiser {
         address creator;
         bool goalMet;
         bool creatorPaid;
+        FundraiserState state;
     }
 
     /// STATE VARIABLES
@@ -27,9 +37,37 @@ contract FundraiserContract is IFundraiser {
 
     /// EVENTS
     event FundraiserCreated(address indexed creator, uint256 fundraiserId, uint256 goal, uint256 deadline);
+    event Deposited(address indexed donator, uint256 fundraiserId, uint256 amount);
 
     /// FUNCTIONS
     // EXTERNAL FUNCTIONS
+
+    /**
+     * @notice Deposit ETH to a fundraiser
+     * @notice Cannot deposit after deadline has passed
+     * @param fundraiserId Fundraiser id
+     */
+    function deposit(uint256 fundraiserId) external payable {
+        if (msg.value == 0) {
+            revert FundraiserContract__DepositCannotBeZero();
+        }
+
+        Fundraiser memory fundraiser = s_fundraisers[fundraiserId];
+
+        if (fundraiser.state == FundraiserState.NOT_CREATED) {
+            revert FundraiserContract__FundraiserDoesNotExist();
+        }
+        if (block.timestamp >= fundraiser.deadline) {
+            revert FundraiserContract__CannotDepositAfterDeadline();
+        }
+        if (s_donators[fundraiserId][msg.sender] == 0) {
+            s_donatorFundraiserIds[msg.sender].push(fundraiserId);
+        }
+
+        s_donators[fundraiserId][msg.sender] += msg.value;
+        s_fundraisers[fundraiserId].amountRaised += msg.value;
+        emit Deposited(msg.sender, fundraiserId, msg.value);
+    }
 
     /**
      * @inheritdoc IFundraiser
@@ -44,7 +82,15 @@ contract FundraiserContract is IFundraiser {
         }
 
         fundraiserId = s_fundraiserQuantity;
-        s_fundraisers[fundraiserId] = Fundraiser(goal, deadline, 0, msg.sender, false, false);
+        s_fundraisers[fundraiserId] = Fundraiser({
+            goal: goal,
+            deadline: deadline,
+            amountRaised: 0,
+            creator: msg.sender,
+            goalMet: false,
+            creatorPaid: false,
+            state: FundraiserState.CREATED
+        });
         s_fundraiserQuantity++;
         emit FundraiserCreated(msg.sender, fundraiserId, goal, deadline);
     }
@@ -56,5 +102,9 @@ contract FundraiserContract is IFundraiser {
 
     function getFundraiserQuantity() external view returns (uint256) {
         return s_fundraiserQuantity;
+    }
+
+    function getDonatorFundaisers(address donator) external view returns (uint256[] memory) {
+        return s_donatorFundraiserIds[donator];
     }
 }
--- crowdfunding-erc20-contract/test/FundraiserContractTest.t.sol
+++ crowdfunding-erc20-contract/test/FundraiserContractTest.t.sol
@@ -10,32 +10,46 @@ contract FundraiserContractTest is Test {
     FundraiserContract fundraiserContract;
     address CREATOR = makeAddr("CREATOR");
     address DONATOR = makeAddr("DONATOR");
-    uint256 constant GOAL = 10 ether;
-    uint256 constant DEADLINE = 5 days;
+    uint256 constant GOAL_MAX = 10 ether;
+    uint256 constant GOAL_MIN = 1 ether;
+    uint256 constant DEADLINE_MAX = 10 days;
+    uint256 constant DEADLINE_MIN = 1 days;
+    uint256 constant DONATOR_INITIAL_BALANCE = 100 ether;
 
     event FundraiserCreated(address indexed creator, uint256 fundraiserId, uint256 goal, uint256 deadline);
+    event Deposited(address indexed donator, uint256 fundraiserId, uint256 amount);
 
     function setUp() public {
         fundraiserContract = new FundraiserContract();
+        vm.deal(DONATOR, DONATOR_INITIAL_BALANCE);
+    }
+
+    // Helper Functions
+    function createFundraiser(uint256 goal, uint256 deadline) public returns (uint256, uint256, uint256) {
+        goal = bound(goal, GOAL_MIN, GOAL_MAX);
+        deadline = bound(deadline, DEADLINE_MIN, DEADLINE_MAX);
+
+        vm.prank(CREATOR);
+        uint256 fundraiserId = fundraiserContract.createFundraiser(goal, block.timestamp + deadline);
+        return (fundraiserId, goal, deadline);
     }
 
     // createFundraiser
     function testCreateFundraiserRevetsIfDeadlineIsInThePast() public {
         vm.expectRevert(FundraiserContract.FundraiserContract__DeadlineCannotBeInThePast.selector);
-        fundraiserContract.createFundraiser(GOAL, block.timestamp - 1);
+        fundraiserContract.createFundraiser(GOAL_MAX, block.timestamp - 1);
     }
 
     function testCreateFundraiserRevetsIfGoalIsZero() public {
         vm.expectRevert(FundraiserContract.FundraiserContract__GoalCannotBeZero.selector);
-        fundraiserContract.createFundraiser(0, block.timestamp + DEADLINE);
+        fundraiserContract.createFundraiser(0, block.timestamp + DEADLINE_MAX);
     }
 
-    function testCreateFundraiserCreatesTheFundraiser() public {
-        vm.prank(CREATOR);
-        uint256 fundraiserId = fundraiserContract.createFundraiser(GOAL, block.timestamp + DEADLINE);
+    function testCreateFundraiserCreatesTheFundraiser(uint256 _goal, uint256 _deadline) public {
+        (uint256 fundraiserId, uint256 goal, uint256 deadline) = createFundraiser(_goal, _deadline);
         FundraiserContract.Fundraiser memory fundraiser = fundraiserContract.getFundraiser(fundraiserId);
-        assertEq(fundraiser.goal, GOAL);
-        assertEq(fundraiser.deadline, block.timestamp + DEADLINE);
+        assertEq(fundraiser.goal, goal);
+        assertEq(fundraiser.deadline, block.timestamp + deadline);
         assertEq(fundraiser.amountRaised, 0);
         assertEq(fundraiser.creator, CREATOR);
         assertEq(fundraiser.goalMet, false);
@@ -46,7 +60,55 @@ contract FundraiserContractTest is Test {
     function testCreateFundraiserEmitsEvent() public {
         vm.prank(CREATOR);
         vm.expectEmit(true, false, false, false, address(fundraiserContract));
-        emit FundraiserCreated(CREATOR, 0, GOAL, block.timestamp + DEADLINE);
-        fundraiserContract.createFundraiser(GOAL, block.timestamp + DEADLINE);
+        emit FundraiserCreated(CREATOR, 0, GOAL_MAX, block.timestamp + DEADLINE_MAX);
+        fundraiserContract.createFundraiser(GOAL_MAX, block.timestamp + DEADLINE_MAX);
+    }
+
+    // deposit
+
+    function testDepositRevertsIfAmountIsZero(uint256 _goal, uint256 _deadline) public {
+        (uint256 fundraiserId,,) = createFundraiser(_goal, _deadline);
+        vm.prank(DONATOR);
+        vm.expectRevert(FundraiserContract.FundraiserContract__DepositCannotBeZero.selector);
+        fundraiserContract.deposit{ value: 0 }(fundraiserId);
+    }
+
+    function testDepositRevetsIfFunraiserDoesNotExist() public {
+        vm.prank(DONATOR);
+        vm.expectRevert(FundraiserContract.FundraiserContract__FundraiserDoesNotExist.selector);
+        fundraiserContract.deposit{ value: GOAL_MAX }(0);
+    }
+
+    function testDepositRevertsIfDeadlineHasPassed(uint256 _goal, uint256 _deadline) public {
+        (uint256 fundraiserId, uint256 goal, uint256 deadline) = createFundraiser(_goal, _deadline);
+        vm.prank(DONATOR);
+        vm.warp(block.timestamp + deadline + 1);
+        vm.expectRevert(FundraiserContract.FundraiserContract__CannotDepositAfterDeadline.selector);
+        fundraiserContract.deposit{ value: goal }(fundraiserId);
+    }
+
+    function testDepositUpdatesState(uint256 _goal, uint256 _deadline, uint256 deposit) public {
+        (uint256 fundraiserId,,) = createFundraiser(_goal, _deadline);
+        deposit = bound(deposit, GOAL_MIN, (GOAL_MAX - 1) / 2);
+        vm.prank(DONATOR);
+        fundraiserContract.deposit{ value: deposit }(fundraiserId);
+        assertEq(fundraiserContract.getFundraiser(fundraiserId).amountRaised, deposit);
+        assertEq(fundraiserContract.getDonatorFundaisers(DONATOR)[0], fundraiserId);
+        assertEq(address(fundraiserContract).balance, deposit);
+
+        vm.prank(DONATOR);
+        fundraiserContract.deposit{ value: deposit }(fundraiserId);
+        assertEq(fundraiserContract.getFundraiser(fundraiserId).amountRaised, deposit * 2);
+        assertEq(fundraiserContract.getDonatorFundaisers(DONATOR).length, 1);
+        assertEq(address(fundraiserContract).balance, deposit * 2);
+    }
+
+    function testDepositEmitsEvent(uint256 _goal, uint256 _deadline, uint256 deposit) public {
+        (uint256 fundraiserId,,) = createFundraiser(_goal, _deadline);
+        deposit = bound(deposit, GOAL_MIN, (GOAL_MAX - 1) / 2);
+        vm.prank(DONATOR);
+        vm.expectEmit(true, false, false, false, address(fundraiserContract));
+        emit Deposited(DONATOR, fundraiserId, deposit);
+        fundraiserContract.deposit{ value: deposit }(fundraiserId);
     }
 }
